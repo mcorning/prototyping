afc562850d8b0f345b5ea929b0acb0a52c6148f5
Michael Corning <michael@secours.io>
2020-08-31 18:22:17 -0700

    forgot $ on line 160 Room.vue

--- src/components/Room.vue
+++ src/components/Room.vue
@@ -157,7 +157,7 @@
     <v-system-bar color="secondary">
       <!-- <v-icon small>mdi-transit-connection-variant </v-icon> -->
 
-      <span class="small">Socket: {{ this.socket.id }}</span>
+      <span class="small">Socket: {{ this.$socket.id }}</span>
       <v-spacer></v-spacer>
       <v-btn @click="testSocket" text><v-icon>mdi-test-tube</v-icon></v-btn>
     </v-system-bar>
