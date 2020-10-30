ea84d47e5502bdfdf048812bbf1fc897ae899de4
Michael Corning <michael@secours.io>
2020-10-30 15:58:57 -0700

    updated build number.  var local=false for docker

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-Vue.prototype.$build = '10.06.16.27';
-let local = true; // as soon as local changes to true, the server will start responding
+Vue.prototype.$build = '10.30.15.54';
+let local = false; // as soon as local changes to true, the server will start responding
 
 import Vue from 'vue';
 import App from './App.vue';
